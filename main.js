3ee9f82bdccc017549d0e1fff062a8aff41a5ae9
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-03-27 12:30:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ function draw(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
         console.log("nose x = " +noseX)
         console.log("nose y = " +noseY )
     }
